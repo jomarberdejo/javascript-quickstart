32bf6616722d667eb9d4e63ba876ff932cc99848
jomarberdejo <115414791+jomarberdejo@users.noreply.github.com>
2023-05-11 12:43:45 +0800

    Update toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -1,1 +1,24 @@
+/*Burger menu for sidebar*/
+const sidebar = document.querySelector('.sidebar');
+
+document.addEventListener('click', event => {
+  if (event.target.closest('.sidebar-toggle, .sidebar') === null) {
+    sidebar.classList.remove('open');
+  }
+});
+
+document.querySelector('.sidebar-toggle').addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+});
+
+
+
+//Code for about-contents
+const aboutContentTitle = document.querySelectorAll(".about-content-title");
+
+aboutContentTitle.forEach((title) => {
+  title.addEventListener("click", () => {
+    title.nextElementSibling.classList.toggle("show");
+  });
+});
 
