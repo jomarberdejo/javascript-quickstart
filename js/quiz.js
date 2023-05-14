fe7e71fa59163ddda04525f0405e1dd92f8d4dcc
jomarberdejo <115414791+jomarberdejo@users.noreply.github.com>
2023-05-14 15:05:59 +0800

    Add files via upload

--- js/quiz.js
+++ js/quiz.js
@@ -1,57 +1,62 @@
+quizForm = document.getElementById("quiz-container");
+const submitBtn = document.getElementById("submit-btn");
+const resetBtn = document.getElementById("reset-btn");
 
-quizForm = document.getElementById('quiz-container');
-const submitBtn = document.getElementById('submit-btn');
-const resetBtn = document.getElementById('reset-btn');
+const questions = document.querySelectorAll(".question");
 
-const questions = document.querySelectorAll('.question');
-
-let score = parseInt(localStorage.getItem('score')) || 0;
+let score = parseInt(localStorage.getItem("score")) || 0;
 
 window.onload = () => {
   // Check if the user has already submitted the quiz
-  const hasSubmitted = localStorage.getItem('hasSubmitted');
+  const hasSubmitted = localStorage.getItem("hasSubmitted");
   if (hasSubmitted) {
-    const currentScore = parseInt(localStorage.getItem('score')) || 0;
+    const currentScore = parseInt(localStorage.getItem("score")) || 0;
     const scorePercentage = (score / questions.length) * 100;
     Swal.fire({
-      title: 'Welcome back!',
+      title: "Welcome back!",
       text: `Your current score is ${currentScore} (${scorePercentage}%)`,
-      icon: 'info'
+      icon: "info",
     });
   }
 };
 
-
-
-
-submitBtn.addEventListener('click', event => {
+submitBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  
+
   // Check if the user has already submitted the quiz
-  const hasSubmitted = localStorage.getItem('hasSubmitted');
+  const hasSubmitted = localStorage.getItem("hasSubmitted");
   if (hasSubmitted) {
     Swal.fire({
-      title: 'You have already submitted the quiz.',
-      icon: 'warning'
+      title: "You have already submitted the quiz.",
+      icon: "warning",
     });
     return;
   }
-  
+
   let unanswered = false;
-  
 
-  questions.forEach(question => {
-      const selectedAnswer = question.querySelector(':checked');
+  questions.forEach((question) => {
+    const selectedAnswer = question.querySelector(":checked");
 
-      if (selectedAnswer === null) {
-        unanswered = true;
-        return;
-      }
+    if (selectedAnswer === null) {
+      unanswered = true;
+      return;
+    }
 
-      const correctAnswer = selectedAnswer.value === 'correct1' || selectedAnswer.value === 'correct2' || selectedAnswer.value === 'correct3' || selectedAnswer.value === 'correct4' || selectedAnswer.value === 'correct5' || selectedAnswer.value === 'correct6' || selectedAnswer.value === 'correct7' || selectedAnswer.value === 'correct8' || selectedAnswer.value === 'correct9' || selectedAnswer.value === 'correct10'; // Example correct answers
-      if (correctAnswer) {
-          score++;
-      }
+    const correctAnswer =
+      selectedAnswer.value === "correct1" ||
+      selectedAnswer.value === "correct2" ||
+      selectedAnswer.value === "correct3" ||
+      selectedAnswer.value === "correct4" ||
+      selectedAnswer.value === "correct5" ||
+      selectedAnswer.value === "correct6" ||
+      selectedAnswer.value === "correct7" ||
+      selectedAnswer.value === "correct8" ||
+      selectedAnswer.value === "correct9" ||
+      selectedAnswer.value === "correct10"; // Example correct answers
+    if (correctAnswer) {
+      score++;
+    }
   });
 
   if (unanswered) {
@@ -63,34 +68,34 @@ submitBtn.addEventListener('click', event => {
 
   if (score === questions.length) {
     Swal.fire({
-      background: 'gold',
+      background: "gold",
       title: `<p style= "color: #000; font-weight: 500">Perfect! </p>`,
       html: `<p style= "color: #000"> Congratulations! You got ${score} (${scorePercentage}%) out of ${questions.length} questions correct! <br> <br> Result: Pass.</p>`,
-      icon: 'success',
-      iconColor: '#fff',
+      icon: "success",
+      iconColor: "#000",
     });
-  } else if(score >= 5) {
+  } else if (score >= 5) {
     Swal.fire({
-      title: ` <p style= "color: #fff">Not bad! </p>` ,
+      title: ` <p style= "color: #fff">Not bad! </p>`,
       html: ` <p style= "color: #fff; font-weight: 500"> Congratulations! You answered ${score} (${scorePercentage}%) out of 10 questions correctly. <br> <br> Result: Pass. </p> `,
-      icon: 'success',
-      background: 'green',
-      iconColor: '#fff',
+      icon: "success",
+      background: "green",
+      iconColor: "#fff",
     });
   } else {
     Swal.fire({
-      background: 'red',
+      background: "red",
       title: `<p style= "color: #fff">Sorry! </p>`,
       html: `<p style= "color: #fff; font-weight: 500">You got ${score} (${scorePercentage}%) out of ${questions.length} questions correct. Please try again. <br> <br> Result: Fail. </p>`,
-      icon: 'error',
-      iconColor: '#fff',
+      icon: "error",
+      iconColor: "#fff",
     });
   }
 
   // Store the score and submission flag in local storage
-  localStorage.setItem('score', score.toString());
-  localStorage.setItem('hasSubmitted', 'true');
-  
+  localStorage.setItem("score", score.toString());
+  localStorage.setItem("hasSubmitted", "true");
+
   submitBtn.disabled = true;
 });
 
@@ -100,22 +105,62 @@ submitBtn.addEventListener('click', event => {
 }
 */
 
-resetBtn.addEventListener('click', event => {
-  if (confirm("All progress will be reset, continue?")=== true){
-    
-    const questions = document.querySelectorAll('.question');
-    const shuffledQuestions = Array.from(questions).sort(() => Math.random() - 0.5); // Shuffle the questions
-    const quizContainer = document.getElementById('quiz-container');
+resetBtn.addEventListener("click", (event) => {
+  if (confirm("All progress will be reset, continue?") === true) {
 
-    shuffledQuestions.forEach(question => {
+
+    const questions = document.querySelectorAll(".question");
+    const shuffledQuestions = Array.from(questions).sort(
+      () => Math.random() - 0.5
+    ); // Shuffle the questions
+    const quizContainer = document.getElementById("quiz-container");
+
+    shuffledQuestions.forEach((question) => {
       quizContainer.appendChild(question); // Re-append the shuffled questions to the quiz container
+    });
+
+    questions.forEach((question) => {
+      const answerOptions = question.querySelectorAll("input[type=radio]");
+      answerOptions.forEach((option) => {
+        option.parentNode.removeAttribute("style"); // Remove the inline style attribute
+        option.disabled = false; // Enable the radio button
+        option.checked = false; // Uncheck the radio button
+      });
     });
 
     quizForm.reset();
     submitBtn.disabled = false;
-    localStorage.removeItem('score'); // clear the stored score from local storage
-    localStorage.removeItem('hasSubmitted');
-    score = 0; 
-    
+    localStorage.removeItem("score"); // clear the stored score from local storage
+    localStorage.removeItem("hasSubmitted");
+    score = 0;
   }
 });
+
+const showAnswers = () => {
+  const correctAnswers = [
+    "correct1",
+    "correct2",
+    "correct3",
+    "correct4",
+    "correct5",
+    "correct6",
+    "correct7",
+    "correct8",
+    "correct9",
+    "correct10",
+  ]; // Example correct answers
+  questions.forEach((question, index) => {
+    const answerOptions = question.querySelectorAll("input[type=radio]");
+    answerOptions.forEach((option) => {
+      if (option.value === correctAnswers[index]) {
+        option.parentNode.style.color = "yellow"; // Highlight the correct answer in green
+      }
+
+      option.disabled = true; // Disable the radio button so the user can't change their answer
+    });
+  });
+};
+
+const showAnswersBtn = document.getElementById("show-answers-btn");
+
+showAnswersBtn.addEventListener("click", showAnswers);
