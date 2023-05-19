b7f7bd376f75727402a390af8241ccaab38f504a
jomarberdejo <115414791+jomarberdejo@users.noreply.github.com>
2023-05-19 08:52:15 +0800

    Delete video.js

--- js/video.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  var videoContainer = document.getElementById('video-container');
-  var youtubeVideo = document.getElementById('youtube-video');
-  
-  youtubeVideo.onerror = function() {
-    videoContainer.innerHTML = "Your network doesn't support YouTube.";
-  };
-});
