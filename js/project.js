30325ae73889c4e6d811d050d243a9f8745cfc60
jomarberdejo <115414791+jomarberdejo@users.noreply.github.com>
2023-05-19 16:14:07 +0800

    Add files via upload

---
+++ js/project.js
@@ -0,0 +1,15 @@
+document.getElementById('complexity-filter').addEventListener('change', function() {
+  const selectedComplexity = this.value;
+  const projectItems = document.getElementsByClassName('project-item');
+  
+  for (let i = 0; i < projectItems.length; i++) {
+    let projectItem = projectItems[i];
+    let complexity = projectItem.getAttribute('data-complexity');
+    
+    if (selectedComplexity === 'all' || selectedComplexity === complexity) {
+      projectItem.style.display = 'block';
+    } else {
+      projectItem.style.display = 'none';
+    }
+  }
+});
